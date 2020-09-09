0535a015bd8b22720595139e59af1718ddbe62eb
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-09-08 22:11:06 -0300

    Completion of form development

--- script/main.js
+++ script/main.js
@@ -1,3 +1,11 @@
-document.getElementById('#form-register').addEventListener('submit', (e) => {
-    e.stopPropagation()
-})
+function onlynumber(evt) {
+    let theEvent = evt || window.event;
+    let key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+    //let regex = /^[0-9.,]+$/;
+    let regex = /^[0-9-]+$/;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+}   
